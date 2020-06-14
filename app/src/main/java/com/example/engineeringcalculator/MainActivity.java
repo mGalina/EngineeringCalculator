836ca0bca11b70878ea189163fa97d300a3ad850
Galina Marakina <g.marakina.2gis@gmail.com>
2020-06-14 12:24:16 +0300

    Code completion

--- app/src/main/java/com/example/engineeringcalculator/MainActivity.java
+++ app/src/main/java/com/example/engineeringcalculator/MainActivity.java
@@ -1,21 +1,25 @@
 package com.example.engineeringcalculator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GET_IMAGE_REQUEST_CODE = 9230;
+    private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case R.id.action_settings:
                 Toast.makeText(MainActivity.this, R.string.settings, Toast.LENGTH_LONG).show();
                 Intent intentSetting = new Intent(getApplicationContext(), SettingsActivity.class);
                 startActivity(intentSetting);
                 return true;
-
             case R.id.action_exit:
                 finish();
             default:
@@ -51,7 +53,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GET_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(SettingsActivity.readResult(data));
+            mImage.setImageBitmap(bitmap);
+        }
+    }
+
     private void InitView() {
+        mImage = findViewById(R.id.imageView);
         final TextView resultField = findViewById(R.id.resultField);
 
         Button numeral1 = findViewById(R.id.bt_1);
--- app/src/main/java/com/example/engineeringcalculator/SettingsActivity.java
+++ app/src/main/java/com/example/engineeringcalculator/SettingsActivity.java
@@ -3,14 +3,11 @@ package com.example.engineeringcalculator;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,35 +22,31 @@ import static com.example.engineeringcalculator.R.*;
 public class SettingsActivity extends AppCompatActivity {
 
     private EditText mInputImage;
-    private Button mSaveImage;
-    private ImageView mImage;
 
-    public static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 100;
+    private static final String IMAGE_RESULT = "IMAGE_RESULT";
+    private static final int REQUEST_CODE_PERMISSION_READ_STORAGE = 100;
+
+    public static String readResult(Intent intent) {
+        return intent.getStringExtra(IMAGE_RESULT);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_settings);
 
-        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);//тут должно быть MainActivity
-        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-            LoadImg();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    REQUEST_CODE_PERMISSION_WRITE_STORAGE);
-        }
-
         initViews();
     }
 
-    private void LoadImg() {
+    private void loadImage() {
         if (isExternalStorageReadable()) {
-            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), String.valueOf(mInputImage));
-            mImage = findViewById(id.imageView);
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-            mImage.setImageBitmap(bitmap);
-            Toast.makeText(SettingsActivity.this, "данные сохранены", Toast.LENGTH_LONG).show();
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), mInputImage.getText().toString());
+            if (!file.exists()) {
+                Toast.makeText(SettingsActivity.this, "Файл не найден", Toast.LENGTH_LONG).show();
+                return;
+            }
+            setResult(RESULT_OK, new Intent().putExtra(IMAGE_RESULT, file.getAbsolutePath()));
+            finish();
         }
     }
 
@@ -64,25 +57,32 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_PERMISSION_WRITE_STORAGE) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_PERMISSION_READ_STORAGE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                LoadImg();
+                loadImage();
             } else {
-                Toast.makeText(SettingsActivity.this, "файл не выбран", Toast.LENGTH_LONG).show();
+                Toast.makeText(SettingsActivity.this, "Нужны разрешения", Toast.LENGTH_LONG).show();
             }
         }
     }
 
     private void initViews() {
         mInputImage = findViewById(id.input_image);
-        mSaveImage = findViewById(id.btn_save);
+        Button mSaveImage = findViewById(id.btn_ok);
 
         mSaveImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //тут должен быть какой-то метод.
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                int permissionStatus = ContextCompat.checkSelfPermission(SettingsActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+                if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+                    loadImage();
+                } else {
+                    ActivityCompat.requestPermissions(SettingsActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_PERMISSION_READ_STORAGE);
+                }
             }
         });
     }
